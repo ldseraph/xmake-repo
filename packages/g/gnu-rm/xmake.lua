c2f5b8d580217d9f0d9a3f3efe2be4e5bb2373e7
ldsereph <ld.seraph@163.com>
2021-11-21 22:54:19 +0800

    feat: :wrench: update gun-rm version to 10.3-2021.10

--- packages/g/gnu-rm/xmake.lua
+++ packages/g/gnu-rm/xmake.lua
@@ -5,27 +5,27 @@ package("gnu-rm")
     set_description("GNU Arm Embedded Toolchain")
 
     if is_host("windows") then
-        set_urls("https://developer.arm.com/-/media/Files/downloads/gnu-rm/$(version)-major-win32.zip", {version = function (version)
-            return version:gsub("%-", ""):gsub("%.", "-") .. "/gcc-arm-none-eabi-" .. version:gsub("%.", "-")
+        set_urls("https://developer.arm.com/-/media/Files/downloads/gnu-rm/$(version)-win32.zip", {version = function (version)
+            return version .. "/gcc-arm-none-eabi-" .. version
         end})
-        add_versions("10.2020-q4", "90057b8737b888c53ca5aee332f1f73c401d6d3873124d2c2906df4347ebef9e")
+        add_versions("10.3-2021.10", "d287439b3090843f3f4e29c7c41f81d958a5323aecefcf705c203bfd8ae3f2e7")
     elseif is_host("linux") then
         if os.arch() == "arm64" then
-            set_urls("https://developer.arm.com/-/media/Files/downloads/gnu-rm/$(version)-major-aarch64-linux.tar.bz2", {version = function (version)
-                return version:gsub("%-", ""):gsub("%.", "-") .. "/gcc-arm-none-eabi-" .. version:gsub("%.", "-")
+            set_urls("https://developer.arm.com/-/media/Files/downloads/gnu-rm/$(version)-aarch64-linux.tar.bz2", {version = function (version)
+                return version .. "/gcc-arm-none-eabi-" .. version
             end})
-            add_versions("10.2020-q4", "343d8c812934fe5a904c73583a91edd812b1ac20636eb52de04135bb0f5cf36a")
+            add_versions("10.3-2021.10", "f605b5f23ca898e9b8b665be208510a54a6e9fdd0fa5bfc9592002f6e7431208")
         else
-            set_urls("https://developer.arm.com/-/media/Files/downloads/gnu-rm/$(version)-major-x86_64-linux.tar.bz2", {version = function (version)
-                return version:gsub("%-", ""):gsub("%.", "-") .. "/gcc-arm-none-eabi-" .. version:gsub("%.", "-")
+            set_urls("https://developer.arm.com/-/media/Files/downloads/gnu-rm/$(version)-x86_64-linux.tar.bz2", {version = function (version)
+                return version .. "/gcc-arm-none-eabi-" .. version
             end})
-            add_versions("10.2020-q4", "21134caa478bbf5352e239fbc6e2da3038f8d2207e089efc96c3b55f1edcd618")
+            add_versions("10.3-2021.10", "97dbb4f019ad1650b732faffcc881689cedc14e2b7ee863d390e0a41ef16c9a3")
         end
     elseif is_host("macosx") then
-        set_urls("https://developer.arm.com/-/media/Files/downloads/gnu-rm/$(version)-major-mac.tar.bz2", {version = function (version)
-            return version:gsub("%-", ""):gsub("%.", "-") .. "/gcc-arm-none-eabi-" .. version:gsub("%.", "-")
+        set_urls("https://developer.arm.com/-/media/Files/downloads/gnu-rm/$(version)-mac.tar.bz2", {version = function (version)
+            return version .. "/gcc-arm-none-eabi-" .. version
         end})
-        add_versions("10.2020-q4", "bed12de3565d4eb02e7b58be945376eaca79a8ae3ebb785ec7344e7e2db0bdc0")
+        add_versions("10.3-2021.10", "fb613dacb25149f140f73fe9ff6c380bb43328e6bf813473986e9127e2bc283b")
     end
 
     on_install("@windows", "@linux", "@macosx", function (package)
